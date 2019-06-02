c26d826326d870662fd7dc0bcd6f187199d0e738
nqlz <1689613805@qq.com>
2019-06-02 13:15:43 +0800

    init

---
+++ consumer/src/main/java/com/nqlz/consumer/ConsumerApplycation.java
@@ -0,0 +1,12 @@
+package com.nqlz.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplycation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplycation.class, args);
+    }
+}
---
+++ producer/src/main/java/com/nqlz/producer/ProcucerApplycation.java
@@ -0,0 +1,11 @@
+package com.nqlz.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcucerApplycation {
+    public static void main(String[] args) {
+        SpringApplication.run(ProcucerApplycation.class, args);
+    }
+}
